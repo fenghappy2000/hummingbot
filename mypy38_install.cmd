763c5d9c234fd8f192e9be4151d1f65c01288b59
fenghappy2000 <fenghappy2000@163.com>
2022-11-06 23:46:34 +0800

    #

--- mypy38_install.cmd
+++ mypy38_install.cmd
@@ -11,7 +11,47 @@
 
 @rem %PIP3% install coverage==5.5
 
-%PIP3% install intel-openmp==2021.3.0
+@rem %PIP3% install intel-openmp==2021.3.0
+
+@rem %PIP3% install nose==1.3.7
+
+@rem %PIP3% install nose-exclude
+
+@rem %PIP3% install numpy==1.20.1
+
+@rem %PIP3% install pandas==1.2.1
+
+@rem %PIP3% install prompt_toolkit==3.0.20
+
+@rem %PIP3% install pydantic==1.9
+
+@rem %PIP3% install tables==3.6.1
+
+@rem %PIP3% install python-dateutil==2.8.1
+
+@rem %PIP3% install scipy==1.6.2
+
+@rem %PIP3% install six==1.14.0
+
+@rem %PIP3% install sqlalchemy==1.4
+
+@rem %PIP3% install pysqlite3
+
+@rem %PIP3% install tabulate==0.8.9
+
+@rem %PIP3% install tzlocal==2.0.0
+
+@rem %PIP3% install ujson==1.35
+
+%PIP3% install zlib
+
+
+
+
+
+
+
+
 
 @pause
 
