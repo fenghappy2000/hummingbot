7cae710a70ee20edd6ce5e7c433b7afa0664c7c7
fenghappy2000 <fenghappy2000@163.com>
2022-11-12 17:25:26 +0800

    #

--- hummingbot/strategy/avellaneda_market_making/MyAvda/MyAvdaContext.py
+++ hummingbot/strategy/avellaneda_market_making/MyAvda/MyAvdaContext.py
@@ -22,14 +22,14 @@ class MyAvdaConfig:
 	risk_factor: float = 0.8  # 0.8/1.0
 	order_amount_shape_factor: float = 0  # 0/0
 	min_spread: float = 0  # 0/0
-	order_refresh_time: float = 5  # 30/60
+	order_refresh_time: float = 30  # 30/60
 	max_order_age: float = 1800.  # 1800/1800
 	order_refresh_tolerance_pct: float = 0  # 0/0
 	filled_order_delay: float = 0.1  # 0.1/60
 	inventory_target_base_pct: float = 50  # 50/50
 	add_transaction_costs: bool = False  # False/False
-	volatility_buffer_size: int = 30  # 60/200
-	trading_intensity_buffer_size: int = 30  # 60/200
+	volatility_buffer_size: int = 60  # 60/200
+	trading_intensity_buffer_size: int = 60  # 60/200
 	order_level_mode: int = 1  # 1/1
 	level_distances: float = 0  # 0/0
 	# order_override = None
